d562819a35a2effb97cc0f46e3255d1f041a7ba2
ADB-DevWorks <adam.brawley@snhu.edu>
2026-09-02 22:19:17 -0500

    Updated UI Added Controls

--- WinUiTextGame/Controls/GameLogControl.xaml.cs
+++ WinUiTextGame/Controls/GameLogControl.xaml.cs
@@ -16,31 +16,13 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace WinUiTextGame.Pages
+namespace WinUiTextGame.Controls
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class TitlePage : Page
+    public sealed partial class GameLogControl : UserControl
     {
-        public TitlePage()
+        public GameLogControl()
         {
             InitializeComponent();
-        }
-
-        private void NewGameLink_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GamePage));
-        }
-
-        private void LoadGame_Click(object sender, RoutedEventArgs e)
-        {
-            //TODO::Implement load link
-        }
-
-        private void OptionsLink_Click(object sender, RoutedEventArgs e)
-        {
-            //TODO:Implement Game Options
         }
     }
 }
